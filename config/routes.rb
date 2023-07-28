c90deee3dfab32b8131c36568b8dcad1014c3435
Damilare <bestbynature2010@gmail.com>
2023-07-28 23:25:28 +0100

    Add the scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :recipe_foods
+  resources :inventory_foods
+  resources :foods
+  resources :recipes
+  resources :inventories
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "foods#index"
 end
