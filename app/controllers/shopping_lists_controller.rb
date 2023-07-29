563ebdeb43cf4124a7c30a8fb237c18e634ed660
Damilare <bestbynature2010@gmail.com>
2023-07-29 10:34:12 +0100

    Add the first part of join table

---
+++ app/controllers/shopping_lists_controller.rb
@@ -0,0 +1,4 @@
+class ShoppingListsController < ApplicationController
+    def generate
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,9 @@ Rails.application.routes.draw do
   resources :foods
   resources :recipes
   resources :inventories
+  resources :users, only: [:index]
 
   get '/public_recipes', to: 'public_recipes#index'
+  get 'generate_shopping_list/:id', to: 'shopping_lists#generate', as: :generate_shopping_list
 
 end
