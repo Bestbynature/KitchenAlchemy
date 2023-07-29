ca2e08253e5bd3b93ad0be6866050df1b8b94224
Damilare <bestbynature2010@gmail.com>
2023-07-30 00:26:23 +0100

    Add the modal

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+  
+  $(document).on('click', '#generateShoppingListBtn', function (event) {
+    event.preventDefault();
+    
+    var selectedInventoryId = $('#inventorySelect').val();
+    if (selectedInventoryId > 0) {
+      window.location.href = '/generate_shopping_list?recipe_id=' + recipeId + '&inventory_id=' + selectedInventoryId;
+    } else {
+      alert('Please select an inventory.');
+    }
+  });
+  
