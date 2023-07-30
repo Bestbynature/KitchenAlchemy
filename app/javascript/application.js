d23bf29587310aa745122c005fa7d5e18fadd924
Damilare <bestbynature2010@gmail.com>
2023-07-30 16:17:38 +0100

    Remove Bootstrap and wrote javascript for the modal

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,14 +1,3 @@
 import "@hotwired/turbo-rails"
 import "controllers"
   
-  $(document).on('click', '#generateShoppingListBtn', function (event) {
-    event.preventDefault();
-    
-    var selectedInventoryId = $('#inventorySelect').val();
-    if (selectedInventoryId > 0) {
-      window.location.href = '/generate_shopping_list?recipe_id=' + recipeId + '&inventory_id=' + selectedInventoryId;
-    } else {
-      alert('Please select an inventory.');
-    }
-  });
-  
